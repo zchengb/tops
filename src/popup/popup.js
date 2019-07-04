7be282afd7f988bf392771f14045a0a4bf86fa6f
Hugo Alliaume <kocal@live.fr>
2019-07-04 14:50:14 +0200

    install ElementUI

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,10 +1,14 @@
 import Vue from 'vue';
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App';
 import store from '../store';
 import router from './router';
 
 global.browser = require('webextension-polyfill');
 Vue.prototype.$browser = global.browser;
+
+Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({
